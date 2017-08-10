257ead859e6604e1f11ca7bdf09bee3e4a491e7d
Brandon Sneed <brsneed@ebay.com>
2017-08-10 11:02:54 -0700

    First real commit.

---
+++ SwiftUtilityLib/SwiftUtilityLib/SwiftUtilityLib.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftUtilityLib.swift
+//  SwiftUtilityLib
+//
+//  Created by Sneed, Brandon on 8/9/17.
+//  Copyright © 2017 __TemplateOrgName__. All rights reserved.
+//
+
+import Foundation
+
+public func test() {
+    print("hello")
+}
---
+++ SwiftUtilityLib/SwiftUtilityLibTests/SwiftUtilityLibTests.swift
@@ -0,0 +1,37 @@
+//
+//  SwiftUtilityLibTests.swift
+//  SwiftUtilityLibTests
+//
+//  Created by Sneed, Brandon on 8/9/17.
+//  Copyright © 2017 __TemplateOrgName__. All rights reserved.
+//
+
+import XCTest
+import SwiftUtilityLib
+
+class SwiftUtilityLibTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        test()
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
