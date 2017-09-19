2b68eb94e498b2c50c7bc3222cfbe6eb34217d45
Brandon Sneed <brsneed@ebay.com>
2017-09-19 15:38:34 -0700

    Added SwiftFeatureLibiOS template, includes resource handling.

---
+++ SwiftFeatureLibiOS/SwiftFeatureLibiOS/SwiftFeatureLibiOS.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftFeatureLibiOS.swift
+//  SwiftFeatureLibiOS
+//
+//  Created by Sneed, Brandon on 9/19/17.
+//  Copyright © 2017 __TemplateOrgName__. All rights reserved.
+//
+
+import Foundation
